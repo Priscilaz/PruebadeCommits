2f9b4b10d0dc1baf996a4098ad4151aa8f83b4b4
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-05-26 16:49:54 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PruebadeCommits
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PruebadeCommits
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
